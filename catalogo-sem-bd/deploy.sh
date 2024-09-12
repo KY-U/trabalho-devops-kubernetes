21331d950c5ef99466518afea63bcbba51dcc874
KYU <uscaio@estudante.ufscar.br>
2024-09-12 00:36:17 -0300

    nada funciona

--- catalogo-sem-bd/deploy.sh
+++ catalogo-sem-bd/deploy.sh
@@ -13,7 +13,7 @@ kubectl apply -f k8s/frontend-service.yaml
 #kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.11.2/deploy/static/provider/cloud/deploy.yaml
 
 # Aplicar o Ingress
-kubectl apply -f k8s/ingress.yaml
+#kubectl apply -f k8s/ingress.yaml
 
 #ao executar o script no windows, ele abre uma janela gitbash e a fecha rapidamente
 read -p $'\nNao some, Jesus te ama! Pressione enter para continuar...'
